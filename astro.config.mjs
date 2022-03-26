bfdd509bba9da9f0ed05a8c8b6292fd81b68c15e
Jorge Meneses Saco <jorgejhms@gmail.com>
2022-03-26 18:27:54 -0500

    cambiando folder a docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
 	// Comment out "renderers: []" to enable Astro's default component support.
+	dist: './docs/',
 	renderers: [],
 });
