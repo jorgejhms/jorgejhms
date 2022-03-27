076864cb8c36e928d5ccda383841478fdf9079ca
Jorge Meneses Saco <jorgejhms@gmail.com>
2022-03-26 20:28:19 -0500

    prueba github

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,11 @@
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
 	// Comment out "renderers: []" to enable Astro's default component support.
-	// projectRoot: 'jorgejhms.net/',
+	
+	dist: './docs/',
 	renderers: [],
+	buildOptions: {
+		// Example: Tell Astro the final URL of your deployed website.
+		   site: 'https://jorgejhms.github.io/jorgejhms/'
+	  }
 });
