17d3fde02a42b0a027ad6ca3ada42e359cc1e566
Jorge Meneses Saco <jorgejhms@gmail.com>
2023-02-24 11:00:21 -0500

    Probando static cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
 
+import { defineConfig } from 'astro/config';
+
 export default {
 	site: 'https://amazing-moonbeam-80387c.netlify.app/'
 }
