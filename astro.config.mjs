4a4b35feefa6982ecf50b21d622bcf2767c1acb4
Jorge Meneses Saco <jorgejhms@gmail.com>
2023-02-24 10:18:03 -0500

    Eliminando código innecesario

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,6 @@
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
 
-// @ts-check
-// export default /** @type {import('astro').AstroUserConfig} */ ({
-// 	// Comment out "renderers: []" to enable Astro's default component support.
-
-// 	// dist: './docs/',
-// 	integrations: [],
-// 	buildOptions: {
-// 		// Example: Tell Astro the final URL of your deployed website.
-// 		site: 'https://amazing-moonbeam-80387c.netlify.app/'
-// 	}
-// });
-
 export default {
 	site: 'https://amazing-moonbeam-80387c.netlify.app/'
 }
